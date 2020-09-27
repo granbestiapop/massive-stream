b76f50b9a9dd813d7ad8b5316601c3d689f9477d
granbestiapop <leonardo.clavijo@mercadolibre.com>
2020-09-27 13:10:44 -0300

    Better approach

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,22 @@
-use futures::stream::StreamExt;
 use futures::future;
+use futures::stream::{StreamExt, TryStreamExt};
 use reqwest::Client;
-use std::time::Duration;
 use tokio::fs::File;
 use tokio::io::{AsyncBufReadExt, BufReader};
 //use tokio::stream::{StreamExt};
-use tokio_util::codec::LinesCodec;
-use tokio_util::codec::Framed;
 use tokio::net::TcpStream;
 use tokio::prelude::*;
+use tokio_util::codec::{Framed, LinesCodec};
+use tokio_util::compat::FuturesAsyncReadCompatExt;
 
-//use tokio::util;
+const CONCURRENT_REQUESTS: usize = 100;
 
-const CONCURRENT_REQUESTS: usize = 10;
-
-
-async fn do_call(client: &Client, url: String) -> Result<String, Box<std::error::Error>> {
+async fn do_call(client: &Client, url: String) -> Result<String, Box<dyn std::error::Error>> {
     let url = format!("http://localhost:8080/a/{}", url);
     let result = client.get(&url).send().await?.text().await?;
-    //tokio::time::delay_for(Duration::from_millis(1000)).await;
+    //tokio::time::delay_for(tokio::time::Duration::from_millis(1000)).await;
+
+    //let result = String::from("");
     Ok(result)
 }
 
@@ -27,42 +25,66 @@ async fn main() {
     let file = File::open("data/foo.txt").await.unwrap();
     let client = Client::new();
 
+    let chunk_size: usize = 1024 * 1024; // 1M
+    let mut total_chunks: u32 = 0;
+    let mut bytes_size: usize = 0;
 
-    let mut stream = TcpStream::connect("127.0.0.1:8080").await.unwrap();
-    stream.write_all(b"GET /a/test HTTP/1.1\r\n\r\n").await.unwrap();
-
-    
-    let frame = Framed::new(stream, LinesCodec::new());
-    frame.map(Result::unwrap)
-        .skip_while(|s|future::ready(!s.is_empty()))
-        .skip_while(|s|future::ready(s.is_empty()))        
-        .for_each(|a| async move{
-            println!("lalalal {:?}", a);
-        }).await;
+    let response = client
+        .get("http://localhost:8080/a/test")
+        .send()
+        .await
+        .unwrap();
+    let http_stream = response
+        .bytes_stream()
+        .map_err(|e| futures::io::Error::new(futures::io::ErrorKind::Other, e))
+        .into_async_read()
+        .compat();
         
+    let file_stream = BufReader::new(file);
 
-    /*
-    let mut stream_http = reqwest::get("http://httpbin.org/ip").await
-        .unwrap()
-        .bytes_stream();*/
-
-    //let frame = Framed::new(stream, LinesCodec::new());
-
-        //.split("/n");
-        
-
-    let cursor = BufReader::new(file)
+    let cursor = http_stream
         .lines()
         .map(Result::unwrap)
-        .map(|st|do_call(&client, st))
+        .map(|line| {
+            let size = line.bytes().len();
+            bytes_size += size;
+            if bytes_size >= chunk_size {
+                bytes_size -= chunk_size;
+                total_chunks += 1;
+            }
+            println!("{}", line);
+            //println!("chunks: {}, bytes:{}", total_chunks, bytes_size);
+            line
+        })
+        .map(|st| do_call(&client, st))
         .buffer_unordered(CONCURRENT_REQUESTS);
 
     cursor
         .for_each(|b| async move {
-            match b {
-                Ok(a) =>  println!("Finish {}", a),
-                Err(err) => println!("Error {:?}", err),
+            if let Err(err) = b {
+                println!("Error {:?}", err);
+                //Ok(a) => println!("Finish {}", a),
+                //Err(err) => println!("Error {:?}", err),
             }
         })
         .await;
+    println!("chunks: {}, bytes:{}", total_chunks, bytes_size);
 }
+
+/*
+let url = "127.0.0.1:8080"; //"raw.githubusercontent.com:843";
+let mut sock_stream = TcpStream::connect(url).await.unwrap();
+sock_stream
+    .write_all(b"GET /a/test HTTP/1.1\r\n\r\n") // /granbestiapop/json-mocks/master/db.json
+    .await
+    .unwrap();
+
+let frame = Framed::new(sock_stream, LinesCodec::new());
+frame
+    .map(Result::unwrap)
+    .skip_while(|s| future::ready(!s.is_empty()))
+    .skip_while(|s| future::ready(s.is_empty()))
+    .for_each(|a| async move {
+        println!("Data from socket {:?}", a);
+    })
+    .await;*/
