280a87a525ac7fa5eeb77f2152d7530869732a26
granbestiapop <leonardo.clavijo@mercadolibre.com>
2020-09-29 21:04:22 -0300

    Add config with params

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,25 @@ use futures::stream::{StreamExt, TryStreamExt};
 use tokio::io::{AsyncBufReadExt};
 use tokio_util::compat::FuturesAsyncReadCompatExt;
 
-const CONCURRENT_REQUESTS: usize = 100;
-const S3_FILE_URL: &str = "http://localhost:8080/stream";
-const UPLOADER_DATA_PATH: &str = "http://localhost:8080/topic";
+#[macro_use]
+extern crate lazy_static;
 
 mod repository;
 use repository::client::RestClient;
 
+const CONCURRENT_REQUESTS: usize = 100;
+const DEFAULT_FILE_URL: &str = "http://localhost:8080/stream";
+const DEFAULT_TARGET: &str = "http://host.docker.internal:8080/topic";
+
+
+lazy_static! {
+    static ref FILE_URL: String = std::env::var("FILE").unwrap_or(DEFAULT_FILE_URL.to_string());
+    static ref TARGET: String = std::env::var("TARGET").unwrap_or(DEFAULT_TARGET.to_string());
+}
+
 async fn do_call(client: &RestClient, body: String) -> Result<String, Box<dyn std::error::Error>> {
     let req = client
-        .request(reqwest::Method::POST, UPLOADER_DATA_PATH)
+        .request(reqwest::Method::POST, TARGET.as_str())
         .body(body)
         .build()?;
     let text = client.execute(req).await?.text().await?;
@@ -21,8 +30,6 @@ async fn do_call(client: &RestClient, body: String) -> Result<String, Box<dyn st
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
-    //let file = File::open(LOCAL_FILE_DIR).await?;
-    //let file_stream = BufReader::new(file);
     let chunk_size: usize = 1024 * 1024; // 1M
     let mut total_chunks: u32 = 0;
     let mut bytes_size: usize = 0;
@@ -30,7 +37,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     let restclient = RestClient::new()?;
 
     let req = restclient
-        .request(reqwest::Method::GET, S3_FILE_URL)
+        .request(reqwest::Method::GET, FILE_URL.as_str())
         .build()?;
     let response =  restclient.stream(req).await?;
     response
--- utils/server/index.js
+++ utils/server/index.js
@@ -4,8 +4,15 @@ const { createReadStream } = require('fs');
 var PORT = process.env.PORT || 8080;
 var app = express();
 
-app.get('/stream', (_req, res) => {
-  const readStream = createReadStream('./data/big.json');
+app.get('/stream', (req, res) => {
+  let options = getRangeOptions(req);
+  const readStream = createReadStream('./data/big.json', options);
+  readStream.pipe(res);
+});
+
+app.get('/stream/small', (req, res) => {
+  let options = getRangeOptions(req);
+  const readStream = createReadStream('./data/small.json', options);
   readStream.pipe(res);
 });
 
@@ -14,6 +21,17 @@ app.post('/topic', (_req, res) => {
   res.send({ test: 'pong' });
 });
 
+
+function getRangeOptions(req){
+  let options = {};
+  const rangeHeader = req.headers.range;
+  if (rangeHeader) { 
+    const range = rangeHeader.split('-');
+    options.start = range[0] ? parseInt(range[0]) : null;
+    options.end = range[1] ? parseInt(range[1]) : undefined;
+  }
+  return options;
+}
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
